2a45eaa645de293dadd1cb174484f20bbed116d9
Branislav Kokavec <Branislav.Kokavec@ibm.com>
2022-11-06 18:57:07 +0100

    add news

--- src/pages/presentations/index.js
+++ src/pages/presentations/index.js
@@ -27,10 +27,39 @@ export default class Presentations extends React.Component {
         </div>
         <section className="section">
           <div className="container">
-            <div className="content">
-              <h2>
+            <div className="content">              
+              <h3>
+                10. novembra 2022 o 16:30h Krajská knižnica v Žiline
+              </h3>
+              <img
+                src="/img/prezentacia_zilina.jpeg"
+                alt="Žilina"
+              />
+              <h3>
+                15. novembra 2022 o 17:00h, V Prednáškovej sále  Univezitnej knižnice v Bratislave Ventúrska 11, Bratislava
+              </h3>
+              <img
+                src="/img/prezentacia_bratislava.jpeg"
+                alt="Bratislava"
+              />
+              <h3>
+                16. novembra 2022 o 11:00h, Univezitná knižnica Prešovskej univerzity v Prešove ul. 17. novembra 1, Prešov
+              </h3>
+              <img
+                src="/img/prezentacia_presov.jpeg"
+                alt="Prešov"
+              />
+              <h3>
+                20. novembra 2022 o 15:00h, KD Zuberec
+              </h3>
+              <img
+                src="/img/prezentacia_zuberec.jpeg"
+                alt="Zuberec"
+              />
+              <h2>Už uskutočnené</h2>
+              <h3>
                 Hložany 26. októbra 2022
-              </h2>
+              </h3>
               <Video 
                 videoSrcURL="https://www.youtube.com/embed/sRkFomI8mo4?start=1"
                 videoTitle="Slovak Canadian Personalities"
